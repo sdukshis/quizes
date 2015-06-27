3196580a314621c703248ac15ae6460bf1072011
sdukshis <sdukshis@gmail.com>
2015-06-27 21:39:56 +0300

    formatting

--- src/incdec.cpp
+++ src/incdec.cpp
@@ -13,22 +13,22 @@ int x = 0;
 
 class Base {
  public:
-    Base() { TRACE_CALL; ++x; }
-    Base(const Base &) { TRACE_CALL; --x; }
-    Base(Base &&) { TRACE_CALL; x += 2; }
-    virtual ~Base() { TRACE_CALL; x -= 3; }
-    Base & operator=(const Base &) { TRACE_CALL; x += 5; return *this; }
-    Base & operator=(Base &&) { TRACE_CALL; x -=2; return *this; }
+    Base()                                      { TRACE_CALL; ++x; }
+    Base(const Base &)                          { TRACE_CALL; --x; }
+    Base(Base &&)                               { TRACE_CALL; x += 2; }
+    virtual ~Base()                             { TRACE_CALL; x -= 3; }
+    Base & operator=(const Base &)              { TRACE_CALL; x += 5; return *this; }
+    Base & operator=(Base &&)                   { TRACE_CALL; x -= 2; return *this; }
 };
 
 class Derived: public Base {
  public:
-    Derived() { TRACE_CALL; x += 2; }
-    Derived(const Derived &other): Base(other) { TRACE_CALL; x += 3; }
-    Derived(Derived &&other): Base(other) { TRACE_CALL; x -= 2; }
-    ~Derived() { TRACE_CALL; --x; }
-    Derived & operator=(const Derived &) { TRACE_CALL; x += 2; return *this; }
-    Derived & operator=(Derived &&) { TRACE_CALL; x -= 3; return *this; }
+    Derived()                                   { TRACE_CALL; x += 2; }
+    Derived(const Derived &other): Base(other)  { TRACE_CALL; x += 3; }
+    Derived(Derived &&other): Base(other)       { TRACE_CALL; x -= 2; }
+    ~Derived()                                  { TRACE_CALL; --x; }
+    Derived & operator=(const Derived &)        { TRACE_CALL; x += 2; return *this; }
+    Derived & operator=(Derived &&)             { TRACE_CALL; x -= 3; return *this; }
 };
 
 int main() try {
